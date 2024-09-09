69aecbdb6a53c039320e8e603f874a3d686154a1
danielore98 <71668153@continental.edu.pe>
2024-09-09 18:01:39 -0500

    Linea de base del proyecto inicial

---
+++ Ejemploo1.py
@@ -0,0 +1,10 @@
+sumando1 = 10
+sumando2 = 5
+suma = sumando1 + sumando2
+
+def multiplicacion(multiplicando, multiplicador):
+    producto = multiplicando * multiplicador
+    return producto
+
+resultado = multiplicacion(sumando1, suma)
+print(resultado)
